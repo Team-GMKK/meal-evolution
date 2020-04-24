f12ec5a30c62e83802bd55a9a9730ecf6c1cb4cc
Mark Go <mark.vince.go@gmail.com>
2020-04-24 11:39:21 -0700

    Add basic setup for React Native Paper with theme for App.

--- index.js
+++ index.js
@@ -4,13 +4,22 @@
 
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#9c1f1f',
+    accent: 'dbc500',
+  },
+};
+
 export default function Main() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <App />
     </PaperProvider>
   );
