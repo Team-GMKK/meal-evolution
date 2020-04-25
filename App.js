ff4df1c6139a54edab6b62ee75dba919ecdc19b2
Mark Go <mark.vince.go@gmail.com>
2020-04-24 22:49:42 -0700

    add RNNavigation dependencies. Backup for Kyle's merges

--- App.js
+++ App.js
@@ -3,5 +3,4 @@ import Skeleton from './Components/Layout/Skeleton';
 
 const App = ({theme}) => <Skeleton theme={theme} />;
 
-
 export default App;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import App from './App';
 import {name as appName} from './app.json';
 
@@ -19,9 +20,11 @@ const theme = {
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
-      <App theme={theme} />
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <App theme={theme} />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
